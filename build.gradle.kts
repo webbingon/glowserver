5246e56f4ed7f392318ff3b8eef2ae77ffc68a10
webbingon <271410277+webbingon@users.noreply.github.com>
2026-09-16 15:47:56 +0900

    chore(build.gradle.kts): install ktlint-gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm") version "2.4.10"
     kotlin("plugin.serialization") version "2.4.10"
+    id("org.jlleitschuh.gradle.ktlint") version "14.2.0"
 }
 
 group = "io.github.webbingon"
